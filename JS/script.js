05febb87543d390c869888d4615e3e808e6a3a6d
MrEyelet <marcin.adam.mroczko@gmail.com>
2017-02-22 20:42:58 +0100

    triangle area, moduł 8.4

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,20 @@
-//java Script
-var name = prompt('Your name ?');
-//alert('Hello, ' + name);
-console.log('Hello, ' + name);
-/*zadanie 8.3*/
+//javaScript
+//Zadanie 8.4
+//Pole trójkąta
+
+/*var a = 2,
+	h = 7,
+	triangleArea = a*h/2;
+
+	document.write('Pole trójkąta o podstawie a= '+a+' i wysokości h= '+h+' równa się '+triangleArea);
+	console.log('Pole trójkąta o podstawie a= '+a+' i wysokości h= '+h+' równa się '+triangleArea);*/
+
+
+var a = prompt('Wpisz długość podstawy trójkąta'),
+	h = prompt('Wpisz wysokość trójkąta'),
+	triangleArea = a*h/2;
+
+	alert('Pole trójkąta o podstawie a= '+a+' i wysokości h= '+h+' równa się '+triangleArea);
+
+	document.getElementById('area').innerHTML=('Pole trójkąta o podstawie a= '+a+' i wysokości h= '+h+' równa się '+triangleArea);
+	console.log('Pole trójkąta o podstawie a= '+a+' i wysokości h= '+h+' równa się '+triangleArea);
