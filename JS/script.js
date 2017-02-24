740b073f1df2d8ee7ace128bc3567ea37930e2a8
MrEyelet <marcin.adam.mroczko@gmail.com>
2017-02-24 20:15:03 +0100

    sub-moduł 8.6

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,12 @@
-var a = prompt('Wpisz dowolną wartość dla parametru a'),
-	b = prompt('Wpisz dowolną wartość dla parametru b'),
-	value = (a * a) + (2 * a * b) - (b * b);
-	console.log(value);
-
-	if (value > 0) {
-		alert('Wynik jest dodatni');
-		document.getElementById('type-of-calculation').innerHTML = ('Wynik jest dodatni');
-		console.log('Wynik jest dodatni');
-	} else if (value < 0) {
-		alert('Wynik jest ujemny');
-		document.getElementById('type-of-calculation').innerHTML = ('Wynik jest ujemny');
-		console.log('Wynik jest ujemny');
-	} else if (value === 0) {
-		alert('Wynik ma wartość równą 0');
-		document.getElementById('type-of-calculation').innerHTML = ('Wynik jest równy 0');
-		console.log('Wynik ma wartość równą 0');
-	} else {
-		alert('Nie wpisałeś poprawnie liczb');
-		document.getElementById('type-of-calculation').innerHTML = ('Nie wpisałeś poprawnie liczb !');
+function getTriangleArea(a, h) {
+	if (a <= 0 || h <= 0) {
+		console.log('Niepoprawne dane, wpisz wartość większą od zera');
 	}
+	return a * h / 2
+}
 
-	console.log('Gdy a = '+ a +' , b = '+ b +' wynik jest równy '+ value);
-	document.getElementById('math-patern').innerHTML=('Gdy a = [ '+ a +' ] , b = [ '+ b +' ] wynik jest równy '+ value);
+var triangleArea1 = getTriangleArea(5,10),
+	triangleArea2 = getTriangleArea(20,10),
+	triangleArea3 = getTriangleArea(5,6);
+
+console.log(triangleArea1, triangleArea2, triangleArea3);
