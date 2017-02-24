ea0ea5f8c2520ba627078411ad93a059bf60f516
MrEyelet <marcin.adam.mroczko@gmail.com>
2017-02-24 17:50:06 +0100

    poprawka

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,24 @@
-var a=prompt('Wpisz dowolną wartość dla parametru a'),
-	b=prompt('Wpisz dowolną wartośc dla parametru b'),
-	value=(a*a) + (2*a*b) - (b*b);
+var a = prompt('Wpisz dowolną wartość dla parametru a'),
+	b = prompt('Wpisz dowolną wartość dla parametru b'),
+	value = (a * a) + (2 * a * b) - (b * b);
 	console.log(value);
 
-	if (value>0) {
+	if (value > 0) {
 		alert('Wynik jest dodatni');
-		document.getElementById('type-of-calculation').innerHTML=('Wynik jest dodatni');
+		document.getElementById('type-of-calculation').innerHTML = ('Wynik jest dodatni');
 		console.log('Wynik jest dodatni');
-	}
-	else if (value<0) {
+	} else if (value < 0) {
 		alert('Wynik jest ujemny');
-		document.getElementById('type-of-calculation').innerHTML=('Wynik jest ujemny');
+		document.getElementById('type-of-calculation').innerHTML = ('Wynik jest ujemny');
 		console.log('Wynik jest ujemny');
-	}
-	else if (value===0) {
+	} else if (value === 0) {
 		alert('Wynik ma wartość równą 0');
-		document.getElementById('type-of-calculation').innerHTML=('Wynik jest równy 0');
+		document.getElementById('type-of-calculation').innerHTML = ('Wynik jest równy 0');
 		console.log('Wynik ma wartość równą 0');
-	}
-	else {
+	} else {
 		alert('Nie wpisałeś poprawnie liczb');
-		document.getElementById('type-of-calculation').innerHTML=('Nie wpisałeś poprawnie liczb !');
+		document.getElementById('type-of-calculation').innerHTML = ('Nie wpisałeś poprawnie liczb !');
 	}
 
-	console.log('Gdy a= '+a+' , b= '+b+' wynik wynosi '+value);
-	document.getElementById('math-patern').innerHTML=('Gdy a= [ '+a+' ] , b= [ '+b+' ] wynik jest równy '+value);
+	console.log('Gdy a = '+ a +' , b = '+ b +' wynik jest równy '+ value);
+	document.getElementById('math-patern').innerHTML=('Gdy a = [ '+ a +' ] , b = [ '+ b +' ] wynik jest równy '+ value);
