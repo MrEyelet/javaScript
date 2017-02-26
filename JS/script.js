bb6292d524ef4d2b93fe82dc3f44c9d8d7fa1559
MrEyelet <marcin.adam.mroczko@gmail.com>
2017-02-26 23:09:44 +0100

    JS 8-11

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,13 @@
-var threeButtons = document.getElementsByClassName('btn');
+var btnAdd = document.getElementById('addElem');
+var btnRemove = document.getElementById('removeElem');
+var unorderedList = document.getElementById('list');
+var itemNumber = document.getElementsByTagName('li').length;
 
-for(var button = 0; button < threeButtons.length; button++) {
-	var threeButtonsInnerText = threeButtons[button].innerText;
-	alert(threeButtonsInnerText);
-}
-
-console.log(threeButtons)
+btnAdd.addEventListener('click', function(event) {
+	unorderedList.innerHTML += '<li>' + 'item ' + (((itemNumber) += 1)-1) + '</li>';
+	//alert('Do listy <ul> zostanie dodany ' + itemNumber + ' element <li>');
+});
+btnRemove.addEventListener('click', function(event) {
+	unorderedList.removeChild(unorderedList.childNodes[2]);
+	//alert('Z listy <ul> zostanie usunięty ' + ((itemNumber) -= 1) + ' element <li>');
+});
