478c30c18da04e95c411ae67968ba3b57d9e91b2
MrEyelet <marcin.adam.mroczko@gmail.com>
2017-02-26 13:55:15 +0100

    Js-8.10

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,8 @@
-var text = 'Papuga kosi trawę, unosząc się nad ziemią 15 centymetrów, na czerwonej chmurze danych.';
-var newAnimal = "Zielona Kodilla";
-var btnLeft = document.getElementById('true');
-var btnRight = document.getElementById('false');
+var threeButtons = document.getElementsByClassName('btn');
 
-	newAnimal = newAnimal.toUpperCase();
-	newAnimal = text.replace(text.substr(0, 6), newAnimal);
-	newAnimal = newAnimal.substr(0, newAnimal.length/2);
+for(var button = 0; button < threeButtons.length; button++) {
+	var threeButtonsInnerText = threeButtons[button].innerText;
+	alert(threeButtonsInnerText);
+}
 
-btnLeft.addEventListener('click', function(event) {
-	alert('Nieprawda ! '  + newAnimal);
-	document.getElementById('cut').innerHTML=('Tekst w oknie dialogowym został celowo ucięty');
-});
-btnRight.addEventListener('click', function(event) {
-	alert('Oczywiście, że fałsz bo przecież '  + newAnimal);
-	document.getElementById('cut').innerHTML=('Tekst w oknie dialogowym został celowo ucięty');
-});
-
-
-console.log(newAnimal);
+console.log(threeButtons)
