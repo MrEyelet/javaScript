b565f058141827d44c1d4346365c4d44be4d0cff
MrEyelet <marcin.adam.mroczko@gmail.com>
2017-02-27 21:12:06 +0100

    JS-8.9

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,14 @@
-var btnAdd = document.getElementById('addElem');
-var btnRemove = document.getElementById('removeElem');
-var unorderedList = document.getElementById('list');
-var itemNumber = document.getElementsByTagName('li').length;
 
-btnAdd.addEventListener('click', function(event) {
-	unorderedList.innerHTML += '<li>' + 'item ' + (((itemNumber) += 1)-1) + '</li>';
-	//alert('Do listy <ul> zostanie dodany ' + itemNumber + ' element <li>');
-});
-btnRemove.addEventListener('click', function(event) {
-	unorderedList.removeChild(unorderedList.childNodes[2]);
-	//alert('Z listy <ul> zostanie usunięty ' + ((itemNumber) -= 1) + ' element <li>');
-});
+function christmassTree(christmassTreeRow) {
+    for(var i = 0; i < christmassTreeRow; i++) {
+        var star = 'k'; 
+        //for(var j = 0 ; j < christmassTreeRow - i; j++) {
+           // star += ' '; 
+        //}
+        for(var j = 0; j <= i * 2; j++) {
+           star += '*'; 
+        }
+    console.log(star);
+    }
+}
+christmassTree(4);
