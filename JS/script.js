08d64c99280a114f3ee99912dde9e59ecc507116
Marcin <marcin.adam.mroczko@gmail.com>
2017-02-27 20:55:43 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -5,9 +5,7 @@ var itemNumber = document.getElementsByTagName('li').length;
 
 btnAdd.addEventListener('click', function(event) {
 	unorderedList.innerHTML += '<li>' + 'item ' + (((itemNumber) += 1)-1) + '</li>';
-	//alert('Do listy <ul> zostanie dodany ' + itemNumber + ' element <li>');
 });
 btnRemove.addEventListener('click', function(event) {
 	unorderedList.removeChild(unorderedList.childNodes[2]);
-	//alert('Z listy <ul> zostanie usunięty ' + ((itemNumber) -= 1) + ' element <li>');
 });
