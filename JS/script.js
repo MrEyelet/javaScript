9e1192db59e2e4cfe428d0692192f6a35fb8d6cc
Marcin <marcin.adam.mroczko@gmail.com>
2017-02-28 09:19:06 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -2,9 +2,9 @@
 function christmassTree(christmassTreeRow) {
     for(var i = 0; i < christmassTreeRow; i++) {
         var star = 'k'; 
-        //for(var j = 0 ; j < christmassTreeRow - i; j++) {
-           // star += ' '; 
-        //}
+        for(var j = 0 ; j < christmassTreeRow - i; j++) {
+           star += ' '; 
+        }
         for(var j = 0; j <= i * 2; j++) {
            star += '*'; 
         }
